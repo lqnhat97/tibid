ec753ad4c1939e6ec44a95dd7fe3cf10cddade75
Nhat Le <nhat.le@ubitec.com>
2021-07-17 10:56:42 +0700

    initial

---
+++ src/main/java/org/tibid/TibidApplication.java
@@ -0,0 +1,17 @@
+package org.tibid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TibidApplication {
+
+	/**
+	 * Main method, used to run the application.
+	 *
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(TibidApplication.class, args);
+	}
+}
